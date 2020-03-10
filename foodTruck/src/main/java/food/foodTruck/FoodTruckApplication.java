c729e6b18799afe8e89d04bc3b46d5631975e85c
AnisHDLY <59841109+AnisHDLY@users.noreply.github.com>
2020-03-10 14:46:13 +0100

    remove

--- foodTruck/src/main/java/food/foodTruck/FoodTruckApplication.java
+++ foodTruck/src/main/java/food/foodTruck/FoodTruckApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoodTruckApplication {
-	private int test;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodTruckApplication.class, args);
