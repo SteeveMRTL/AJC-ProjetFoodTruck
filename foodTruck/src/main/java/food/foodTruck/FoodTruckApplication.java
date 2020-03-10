118e69fc8cbda755f5f809df33939b875331fb8d
Steeve <Steeve@192.168.1.108>
2020-03-10 14:19:46 +0100

    ajout d'un attribut test à FoodTruckApplication.java

--- foodTruck/src/main/java/food/foodTruck/FoodTruckApplication.java
+++ foodTruck/src/main/java/food/foodTruck/FoodTruckApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoodTruckApplication {
+	private int test;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodTruckApplication.class, args);
